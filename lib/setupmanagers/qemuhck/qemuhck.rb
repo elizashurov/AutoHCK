2981738b2aea9b655aa5d823cae59947b232564b
Elizabeth Ashurov <eashurov@redhat.com>
2026-05-14 09:00:30 +0300

    RHEL-175830: [qemuhck] add TPM state support
    Add tpm_state option to OPT_NAMES, set its default to false in
    qemu_machine.json, and map tpm_state=true to the tpm-tis device
    in states.json (replacing the fw_type/uefi approach).
    Now TPM is a platform requirement, not a firmware one.
    
    Signed-off-by: Elizabeth Ashurov <eashurov@redhat.com>

--- lib/setupmanagers/qemuhck/qemuhck.rb
+++ lib/setupmanagers/qemuhck/qemuhck.rb
@@ -14,7 +14,7 @@ module AutoHCK
 
     QEMUHCK_INFO_LOG_FILE = 'qemuhck.txt'
     OPT_NAMES = %w[viommu_state s3_state s4_state enlightenments_state vhost_state machine_type fw_type cpu
-                   ctrl_net_device vbs_state].freeze
+                   ctrl_net_device vbs_state tpm_state].freeze
 
     def initialize(project)
       initialize_project project
